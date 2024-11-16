95d36af5408f0d212d32a2e86a2289c60a7dde18
iandelah1 <iandelah1@gmail.com>
2024-11-16 15:07:00 -0800

    Updated TestTeleOP

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test.java
@@ -1,5 +1,7 @@
 package org.firstinspires.ftc.teamcode;
 
+import android.graphics.Color;
+
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.CRServo;
@@ -14,6 +16,27 @@ public class Test extends BaseOpMode {
         telemetry.addData("ColorSensor: Red", ColorSensor.red());
         telemetry.addData("ColorSensor: Green", ColorSensor.green());
         telemetry.addData("ColorSensor: Blue", ColorSensor.blue());
+        // Thresholds for color detection
+        int redThreshold = 900;
+        int greenThreshold = 900;
+        int blueThreshold = 750;
+
+        // Detect if the color is red
+        if (ColorSensor.red() > 800 && ColorSensor.red() > ColorSensor.blue() && ColorSensor.red() > ColorSensor.green()) {
+            telemetry.addData("Detected Color", "Red");
+        }
+        // Detect if the color is yellow (requires both red and green)
+        else if (ColorSensor.red() > 900 && ColorSensor.green() > 900) {
+            telemetry.addData("Detected Color", "Yellow");
+        }
+        // Detect if the color is blue
+        else if (ColorSensor.blue() > 500 && ColorSensor.blue() > ColorSensor.red() && ColorSensor.blue() > ColorSensor.green() ) {
+            telemetry.addData("Detected Color", "Blue");
+        }
+        // If no specific color is detected
+        else {
+            telemetry.addData("Detected Color", "Unknown");
+        }
         //if (gamepad1.a) {FR.setPower(.1);}
         //else {FR.setPower(0);}
         //if (gamepad1.b) {FL.setPower(.1);}
@@ -22,11 +45,12 @@ public class Test extends BaseOpMode {
         //else {BR.setPower(0);}
         //if (gamepad1.y) {BL.setPower(.1);}
         //else {BL.setPower(0);}
-        if (gamepad1.a) {IntakeRotate.setPosition(0.75);}
-        else {IntakeRotate.setPosition(0.15);}
-        if (gamepad1.b) {ExtenderRotate.setPosition(0.5);}
-        else {ExtenderRotate.setPosition(0);}
 
+
+        if (gamepad2.right_bumper) {
+            Intake.setPower(0.8);
+        }
+/*
         if (gamepad1.right_bumper) {Intake.setPower(-1);}
 
         else if (ColorSensor.red() > 800) {Intake.setPower(0);}
@@ -42,6 +66,8 @@ public class Test extends BaseOpMode {
         if (gamepad1.dpad_right) {RLIFT.setPower(-.8);LLIFT.setPower(-.8);}
         else {RLIFT.setPower(0);LLIFT.setPower(0);}
         if (gamepad2.a) {SpoonServo.setPosition(.3);}
+
+         */
 
 
     }
